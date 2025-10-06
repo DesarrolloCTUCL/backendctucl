feebfc25d5918941ba3aafc793333d60f9355aa2
CristianDv7 <desarrolloctucl@gmail.com>
2025-10-06 08:49:57 -0500

    se agrega metodo create y update a busline

--- src/modules/bus-line/bus-line.controller.ts
+++ src/modules/bus-line/bus-line.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get ,Post,Body,Param,Patch} from '@nestjs/common';
 import { BusLineService } from './bus-line.service';
+import { CreateBusLineDto } from './dto/bus-line-create.dto';
 
 @Controller('bus-line')
 export class BusLineController {
@@ -9,4 +10,19 @@ export class BusLineController {
   async findAll() {
     return this.busLineService.findAll();
   }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.busLineService.findOne(+id);
+  }
+
+  @Post()
+  async create(@Body() dto: CreateBusLineDto) {
+    return this.busLineService.create(dto);
+  }
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() dto: CreateBusLineDto) {
+    return this.busLineService.update(+id, dto);
+  }
+  
 }
