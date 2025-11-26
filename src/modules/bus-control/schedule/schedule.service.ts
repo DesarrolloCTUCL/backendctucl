44a46b5679e1ec2447dce60a7ad5cc0be17757f2
CristianDv7 <desarrolloctucl@gmail.com>
2025-11-26 09:36:32 -0500

    se cambia schedule service para despachar con vehicleid

--- src/modules/bus-control/schedule/schedule.service.ts
+++ src/modules/bus-control/schedule/schedule.service.ts
@@ -21,7 +21,7 @@ export class ScheduleService {
       // Verificar si ya existe un despacho para el mismo bus en esa fecha
       const existing = await this.scheduleRepository.findOne({
         where: {
-          vehicle:{register:createScheduleDto.vehicle_id} ,
+          vehicle: { id: createScheduleDto.vehicle_id },
           date: scheduleDate,
         },
       });
@@ -35,8 +35,10 @@ export class ScheduleService {
       // Crear y guardar nuevo despacho
       const newSchedule = this.scheduleRepository.create({
         ...createScheduleDto,
-        date: scheduleDate, // ✅ nos aseguramos que siempre vaya en formato Date
+        date: scheduleDate,
+        vehicle: { id: createScheduleDto.vehicle_id } // 👈 AQUÍ SE ASIGNA LA RELACIÓN
       });
+      
   
       return await this.scheduleRepository.save(newSchedule);
   
