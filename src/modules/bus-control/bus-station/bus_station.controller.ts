4a1bffbe0ce6c1568cad3e61cc5ae8e8bb1d452c
CristianDv7 <desarrolloctucl@gmail.com>
2025-11-27 11:11:22 -0500

    se coloca en publico control points

--- src/modules/bus-control/bus-station/bus_station.controller.ts
+++ src/modules/bus-control/bus-station/bus_station.controller.ts
@@ -8,16 +8,18 @@ import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @ApiBearerAuth()
 @UseGuards(AuthGuard)
+
 @Controller('bus-station')
 export class BusStationController {
   constructor(private readonly bus_station_service: BusStationService) {}
 
+  @Public()
   @Post('create')
   create(@Body() createBusStationDto: CreateBusStationDto) {
     return this.bus_station_service.create(createBusStationDto);
   }
 
-
+  @Public()
   @Get('control-points')  // <--- Mover esta ruta fija arriba de la dinámica
   async getControlPoints() {
     console.log('⚡️ Entrando a getControlPoints');
@@ -37,12 +39,12 @@ export class BusStationController {
       };
     }
   }
-
+  @Public()
   @Get()
   getAll() {
     return this.bus_station_service.findAll();
   }
-
+  @Public()
   @Get(':id')   // <--- Ruta dinámica debe ir al final
   getOne(@Param('id') id: number) {
     return this.bus_station_service.findOne(id);
